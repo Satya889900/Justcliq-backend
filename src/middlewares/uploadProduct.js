d8e7fe4ecb3e020f5f4d51c806ae504b9ee17364
SP889900 <satya.prakash.fiem.cse20@teamfuture.in>
2025-12-22 15:57:55 +0530

    set imge error uplode

--- src/middlewares/uploadProduct.js
+++ src/middlewares/uploadProduct.js
@@ -33,7 +33,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "products",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    allowed_formats: ["jpg", "jpeg", "png", "webp" ,"avif"],
   },
 });
 
